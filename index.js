3c8e6f92f0ebef094097f5a1fefd5cbe1d645175
Jazmine <jazmihic@amazon.com>
2023-04-09 11:36:56 -0500

    lesson done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, targetName) {
+  const anotherArray = array.filter(
+    (arr) => arr.toLowerCase() === targetName.toLowerCase()
+  );
+  return anotherArray;
+}
+function fuzzyMatch(array, letter) {
+  const anotherArray = array.filter((arr) => arr.startsWith(letter));
+  return anotherArray;
+}
+function matchName(array, targetName) {
+  const anotherArray = array.filter((arr) => arr.name === targetName);
+  return anotherArray;
+}
